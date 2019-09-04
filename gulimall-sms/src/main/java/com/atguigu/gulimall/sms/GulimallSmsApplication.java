0b88d51f4692852303a27759a6c8e22a98f81529
ygx12130 <www.1124923342@qq.com>
2019-09-04 17:13:14 +0800

    创建其他服务模块3

--- gulimall-sms/src/main/java/com/atguigu/gulimall/sms/GulimallSmsApplication.java
+++ gulimall-sms/src/main/java/com/atguigu/gulimall/sms/GulimallSmsApplication.java
@@ -8,7 +8,7 @@ public class GulimallSmsApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("GulimallSmsApplication 项目启动");
+        System.out.println("GulimallSmsApplication 项目启动2");
         SpringApplication.run(GulimallSmsApplication.class, args);
     }
 
